745ab92944c0f920be0c6ce0bd3fa195ed831617
Linus Hernvall <linus.hernvall@gmail.com>
2022-12-13 17:55:18 +0100

    Lagt till JS-doc i JS.

--- public/javascript/calender-days-clicked.js
+++ public/javascript/calender-days-clicked.js
@@ -2,38 +2,25 @@ window.addEventListener("DOMContentLoaded", main);
 
 /** Adds a start and flow to the functions. */
 function main() {
-  addEventListeners();
+  selectDay();
 }
 
-// FUNKADE FÖR ATT MARKERA DAGAR
-function addEventListeners() {
+/** Ger användaren möjligheten att välja önskad dag i kalendern. */
+function selectDay() {
   const days = document.querySelectorAll(".calendar div");
   for (const day of days) {
-    day.addEventListener("click", toggleDay);
+    day.addEventListener("click", activateDay);
   }
 }
 
 /**
- *
+ * Tar bort och lägger klassen "active" på den valda dagen i kalendern.
+ * Ser till att endast en dag kan vara markerad åt gången.
  * @param {MouseEvent} event
  */
-function toggleDay(event) {
+function activateDay(event) {
   const activeDay = document.querySelector(".calendar div.active");
   activeDay?.classList.remove("active");
   const day = event.currentTarget;
   day.classList.add("active");
-
-  // const day = event.currentTarget;
-  // day.classList.toggle("active");
-
-  // clearClickedDay(event);
 }
-
-// function clearClickedDay(event) {
-//   const daysClicked = document.querySelectorAll(".active");
-
-//   for (const dayClicked of daysClicked) {
-//     if (dayClicked) dayClicked.classList.remove("active");
-//     dayClicked.classList.add("active");
-//   }
-// }
