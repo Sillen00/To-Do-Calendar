0cff1df2a6c21f8af5a9f90203c9b22236743754
Linus Hernvall <linus.hernvall@gmail.com>
2022-12-14 15:32:53 +0100

    Får till todo med knappar, men står som undefined.

--- public/javascript/todos.js
+++ public/javascript/todos.js
@@ -20,17 +20,53 @@ function addTodo() {
   saveTodoButton.addEventListener("click", createNewTodo);
 }
 
+// FUNKADE EJ!!
+// function createNewTodo() {
+//   let input = document.getElementById("todoInput").value;
+//   let todo = document.getElementById("todaysTodo");
+
+//   if (input.value.trim() !== 0) {
+//     let newTodo = document.createElement("div");
+//     newTodo.classList.add("todo");
+// newTodo.innerHTML = `
+// <div class="todo-content">
+//               <p class="todo-title">${input.value}</p>
+//               <p class="todo-date">2022-12-10</p>
+//             </div>
+//             <div class="todo-buttons">
+//               <button class="todo-button-change">Ändra</button>
+//               <button class="todo-button-delete">Ta bort</button>
+//             </div>
+// `;
+//     todo.appendChild(newTodo);
+//     input.value = "";
+//   } else {
+//     alert("Var vänlig skriv en Todo.");
+//   }
+// }
+
 function createNewTodo() {
-  let todo = document.createElement("div");
-  todo.classList.add("todo");
+  let newTodo = document.createElement("div");
+  newTodo.classList.add("todo");
+
   let inputValue = document.getElementById("todoInput").value;
   let todoTitle = document.createTextNode(inputValue);
 
-  todo.appendChild(todoTitle);
+  newTodo.appendChild(todoTitle);
   if (inputValue === "") {
     alert("Du måste skriva någonting...");
   } else {
-    document.getElementById("todaysTodo").appendChild(todo);
+    document.getElementById("todaysTodo").appendChild(newTodo);
+    newTodo.innerHTML = `
+    <div class="todo-content">
+                  <p class="todo-title">${inputValue.value}</p>
+                  <p class="todo-date">2022-12-10</p>
+                </div>
+                <div class="todo-buttons">
+                  <button class="todo-button-change">Ändra</button>
+                  <button class="todo-button-delete">Ta bort</button>
+                </div>
+    `;
   }
   document.getElementById("todoInput").value = "";
 
@@ -49,28 +85,28 @@ function createNewTodo() {
 }
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+// function newElement() {
+//   var li = document.createElement("li");
+//   var inputValue = document.getElementById("myInput").value;
+//   var t = document.createTextNode(inputValue);
+//   li.appendChild(t);
+//   if (inputValue === "") {
+//     alert("You must write something!");
+//   } else {
+//     document.getElementById("myUL").appendChild(li);
+//   }
+//   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
-}
+//   for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function () {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     };
+//   }
+// }
