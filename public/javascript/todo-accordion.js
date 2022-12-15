03aea9c0d4aabd92c731b2bcf7c2022998bc7bed
Simon <sillen.00@hotmail.com>
2022-12-15 12:07:09 +0100

    misslyckat försök.

--- public/javascript/todo-accordion.js
+++ public/javascript/todo-accordion.js
@@ -6,13 +6,40 @@ function main(){
 
 
 
-function toggleTodos(){
-    const todoSections = document.getElementsByClassName("todo-section");
+// function toggleTodos(){
+//     const todoSections = document.getElementsByClassName("todo-section");
 
-    for (let i = 0 ; i < todoSections.length ; i++){
-        todoSections[i].addEventListener("click", function(){
+//     for (let i = 0 ; i < todoSections.length ; i++){
+//         todoSections[i].addEventListener("click", function(){
             
-            this.classList.toggle("accordion")
+//             this.classList.toggle("accordion")
+//         });
+//     }
+// };
+
+
+function toggleTodos(){
+
+    const todoHeading = document.getElementsByClassName("todo-heading");
+    for (let i = 0 ; i < todoHeading.length ; i++){
+        todoHeading[i].addEventListener("click", function(){
+
+            const container = document.querySelector(".todo-container")
+
+            for( let j= 0 ; j < container.length ; j++){
+               
+                if(this.style.display = "block" === true){
+                    this.style.display = "none"
+                }
+                else {
+                    this.style.display = "block"
+                }
+
+                // this.style.display = "none"
+                // container[j].style.display = "none"
+                // container[j].classList.toggle("accordion");
+                // todoHeading[i].classList.toggle("accordion");
+            }
         });
     }
 };
