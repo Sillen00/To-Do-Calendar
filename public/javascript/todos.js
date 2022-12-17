ef31367c8d127050b7c56a7887a8566f2a8bbddb
Linus Hernvall <linus.hernvall@gmail.com>
2022-12-17 10:09:05 +0100

    Ger möjlighet att toggla popup.

--- public/javascript/todos.js
+++ public/javascript/todos.js
@@ -1,8 +1,9 @@
+window.addEventListener("DOMContentLoaded", initTodos);
+
 /** Startar funktionerna för skapandet av Todo.*/
-function initTodos(){
+function initTodos() {
   addEventListeners();
 }
-
 
 /** Aktiverar funktionen togglePopup vid klick av knappen på Skapa Todo.  */
 function addEventListeners() {
@@ -19,52 +20,52 @@ function togglePopup() {
   todoPopup.classList.toggle("show-popup");
 }
 
-/** Skapar en ny Todo. (Just nu  läggs alla Todos till i Dagens Todo.) */
-function createNewTodo() {
-  let newTodo = document.createElement("div");
-  newTodo.classList.add("todo");
+// /** Skapar en ny Todo. (Just nu  läggs alla Todos till i Dagens Todo.) */
+// function createNewTodo() {
+//   let newTodo = document.createElement("div");
+//   newTodo.classList.add("todo");
 
-  let todoInput = document.getElementById("todoInput").value;
-  let dateInput = document.getElementById("dateInput").value;
+//   let todoInput = document.getElementById("todoInput").value;
+//   let dateInput = document.getElementById("dateInput").value;
 
-  let date = new Date();
-  todaysDate = date.toLocaleString("sv-SE");
+//   let date = new Date();
+//   todaysDate = date.toLocaleString("sv-SE");
 
-  if (todoInput === "" || dateInput === "") {
-    alert("Var vänlig skriv en Todo och fyll i ett datum.");
-  } else if (todaysDate.includes(dateInput)) {
-    newTodo.innerHTML = `
-    <div class="todo-content">
-        <p class="todo-title">${todoInput}</p>
-        <p class="todo-date">${dateInput}</p>
-    </div>
-    <div class="todo-buttons">
-        <button class="todo-button-change">Ändra</button>
-        <button class="todo-button-delete">Ta bort</button>
-    </div>
-    `;
-    const todaysTodo = document.getElementById("todaysTodo");
-    todaysTodo.appendChild(newTodo);
+//   if (todoInput === "" || dateInput === "") {
+//     alert("Var vänlig skriv en Todo och fyll i ett datum.");
+//   } else if (todaysDate.includes(dateInput)) {
+//     newTodo.innerHTML = `
+//     <div class="todo-content">
+//         <p class="todo-title">${todoInput}</p>
+//         <p class="todo-date">${dateInput}</p>
+//     </div>
+//     <div class="todo-buttons">
+//         <button class="todo-button-change">Ändra</button>
+//         <button class="todo-button-delete">Ta bort</button>
+//     </div>
+//     `;
+//     const todaysTodo = document.getElementById("todaysTodo");
+//     todaysTodo.appendChild(newTodo);
 
-    const allTodo = document.getElementById("allTodo");
-    allTodo.appendChild(newTodo.cloneNode(true));
+//     const allTodo = document.getElementById("allTodo");
+//     allTodo.appendChild(newTodo.cloneNode(true));
 
-    togglePopup("click");
-  } else {
-    newTodo.innerHTML = `
-    <div class="todo-content">
-        <p class="todo-title">${todoInput}</p>
-        <p class="todo-date">${dateInput}</p>
-    </div>
-    <div class="todo-buttons">
-        <button class="todo-button-change">Ändra</button>
-        <button class="todo-button-delete">Ta bort</button>
-    </div>
-    `;
-    allTodo.appendChild(newTodo);
+//     togglePopup("click");
+//   } else {
+//     newTodo.innerHTML = `
+//     <div class="todo-content">
+//         <p class="todo-title">${todoInput}</p>
+//         <p class="todo-date">${dateInput}</p>
+//     </div>
+//     <div class="todo-buttons">
+//         <button class="todo-button-change">Ändra</button>
+//         <button class="todo-button-delete">Ta bort</button>
+//     </div>
+//     `;
+//     allTodo.appendChild(newTodo);
 
-    togglePopup("click");
-  }
-  document.getElementById("todoInput").value = "";
-  document.getElementById("dateInput").value = "";
-}
+//     togglePopup("click");
+//   }
+//   document.getElementById("todoInput").value = "";
+//   document.getElementById("dateInput").value = "";
+// }
